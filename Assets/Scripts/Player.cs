ee0558a2dcac99c5e4372048a434053c091be15a
Santiago Santos <santiago_sabad@hotmail.com>
2022-06-01 02:16:33 -0500

    Animations Bug Fixed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aad4d1d86ddb21e322578bd5d4a35d04d80228ee9b4794b5eac205c85cb8eebe
-size 3885
+oid sha256:a3c56bc553fe98694a6b4254cf3ac44c3baef2ab4d53696dc765a40854903729
+size 3984
--- Assets/Scripts/PlayerBullet.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d2d7b085fa3c3655e74fbc286b86672886c3236d78518813070097ba16135ef7
-size 1249
